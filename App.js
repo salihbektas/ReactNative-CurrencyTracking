f6db792ed8a8e6dfcbb14339056eba931ce62dfd
Salih Bektaş <sbektas@gtu.edu.tr>
2022-09-07 13:10:02 +0300

    init & fetch data

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
+import axios from 'axios';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  function getir() {
+    axios.get("https://api.exchangerate.host/latest")
+    .then(response => console.log(response.data.rates))
+    .catch(error => console.error(error));
+  }
+
+
   return (
     <View style={styles.container}>
+      <Button title="Getir" onPress={getir} />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
